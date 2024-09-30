07f7cd9f602d7242ccd1be696f5781a1d1a705cd
gggfox <gerardogalangarzafox@gmail.com>
2024-09-30 09:15:25 -0600

    fix: update shared package

---
+++ services/shared/src/userSchema.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const userSchema = z.object({
+  name: z
+    .string()
+    .min(1, "Name is required")
+    .max(64, "Name cant exceed 64 characters"),
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters")
+    .max(128),
+});
